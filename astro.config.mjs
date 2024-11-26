5fa49f12412f67d7b616b31a3480d8683004ff28
Zade Viggers <zade@viggers.net>
2024-11-27 01:57:38 +1300

    FIx updated badge

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,22 +4,22 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://mcdocs.zade.viggers.net/",
-	integrations: [
-		starlight({
-			title: "MC:JE Technical Docs",
-			social: {
-				github: "https://github.com/zadeviggers/mc-java-command-docs",
-			},
-			components: {
-				SiteTitle: "./src/components/SiteTitle.astro",
-			},
-			sidebar: [
-				{
-					label: "Components",
-					autogenerate: { directory: "components" },
-				},
-			],
-		}),
-	],
+  site: "https://mcdocs.zade.viggers.net/",
+  integrations: [
+    starlight({
+      title: "MC:JE Reference Docs",
+      social: {
+        github: "https://github.com/zadeviggers/mc-java-command-docs",
+      },
+      components: {
+        SiteTitle: "./src/components/SiteTitle.astro",
+      },
+      sidebar: [
+        {
+          label: "Components",
+          autogenerate: { directory: "components" },
+        },
+      ],
+    }),
+  ],
 });
